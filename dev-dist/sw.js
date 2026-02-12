83002baebd49c0c4eea37c3d308c4fd37ecdf797
Christian <cedonoso@espe.edu.ec>
2026-02-12 12:18:46 -0500

    feat: actualizar la revisión del archivo index.html en el service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5ttmr5n7dps"
+    "revision": "0.duvflnb2hgo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
